24dc1168e691cfda0c47c88550adb8cf9444f7c7
Peter Weinberg <pweinberg633@gmail.com>
2017-03-04 13:29:14 -0500

    add styles dirrectory

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import rootReducer from './rootReducer';
 import thunk from 'redux-thunk';
-import './index.css';
+import './styles/index.css';
 
 const store = createStore(
   rootReducer,
