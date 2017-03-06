9bea9ac2b204a08ac219d497618c295ce582722d
Sean Smith <sean.smith.2009@gmail.com>
2017-03-05 23:43:14 -0800

    updated auth features

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,27 @@
 import express from 'express';
+import passport from 'passport';
+
 const router = express.Router();
+
+// example of an authentication middleware:
+function isAuthenticated(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
 
 router.get('/api/user', (req, res) => {
   res.send(req.user);
+});
+
+router.post('/api/verify-credentials', isAuthenticated, (req, res) => {
+  const { username } = req.body;
+  console.log(`processing verification for ${username}`);
+  // process verification with FCC here ...
+
+  res.sendStatus(201);
 });
 
 export default router;
--- src/actions/loginActions.js
+++ src/actions/loginActions.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
 export function getUserData() {
   return axios.get('/api/user').then(res => { return res.data });
+}
+
+export function verifyUser(username) {
+  return axios.post('/api/verify-credentials', { username } );
 }
