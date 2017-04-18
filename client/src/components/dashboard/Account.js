3784cb245c7db94619474fbcba26128c484b4fe9
Peter Weinberg <pweinberg633@gmail.com>
2017-04-18 10:34:43 -0400

    allow only one flash message at a time, fix account page issue

--- client/src/components/dashboard/Account.js
+++ client/src/components/dashboard/Account.js
@@ -21,7 +21,7 @@ const ConfirmModal = ({ open, input, handleInput, deleteUser, close }) => {
 
       <Modal.Header style={{ background: 'rgb(225,225,225)' }}>
         <h1>
-          <span>Type your username to confirm:</span>
+          <span><i className="red warning sign icon" />Type your username to confirm:</span>
         </h1>
       </Modal.Header>
 
@@ -64,6 +64,22 @@ class Account extends React.Component {
     modal: false,
     flashMessageCleared: false
   }
+  componentWillMount() {
+    // bit of a hack here in CWM, but per comment on 127, just manually removing this node
+    // from dom (if it exists), since there should never be a flash message rendered when
+    // first rendering this route. This will only need to happen if user does not manually
+    // clear whatever flash message is there before switching routes.
+    document.getElementsByClassName('flashMessage')[0] &&
+    document.getElementsByClassName('flashMessage')[0].remove();
+  }
+  componentDidMount = () => document.addEventListener('click', this.handleClick);
+  componentWillUnmount = () => document.removeEventListener('click', this.handleClick)
+  // 'close' is className of close icon in flash message
+  handleClick = (e) => {
+    e.target.classList.contains('close')
+    ? this.setState({ flashMessageCleared: true })
+    : this.setState({ flashMessageCleared: false });
+  }
   deleteUser = (e) => {
     if (e) e.preventDefault();
     const { input } = this.state;
@@ -102,17 +118,9 @@ class Account extends React.Component {
   }
   handleInput = (e) => this.setState({ input: e.target.value });
   toggleModal = () => this.setState({ modal: !this.state.modal });
-  componentDidMount = () => document.addEventListener('click', this.handleClick);
-  componentWillUnmount = () => document.removeEventListener('click', this.handleClick);
-  // 'close' is className of close icon in flash message
-  handleClick = (e) => e.target.classList.contains('close')
-    ? this.setState({ flashMessageCleared: true })
-    : this.setState({ flashMessageCleared: false });
-
   render() {
     const { isDesktop } = this.props.screen;
     const { flashMessageCleared } = this.state;
-    console.log(document.getElementsByClassName('flashMessage'))
     // handles margins when flash messages are rendered and
     // also when subsequently removed. handleClick and flashMessageCleared state of
     // this component are all related to this.
--- client/src/reducers/flashMessages.js
+++ client/src/reducers/flashMessages.js
@@ -1,34 +1,22 @@
-import { ADD_FLASH_MESSAGE, DELETE_FLASH_MESSAGE, CLEAR_FLASH_MESSAGE } from '../actions/types';
-import shortid from 'shortid';
+import { ADD_FLASH_MESSAGE, CLEAR_FLASH_MESSAGE } from '../actions/types';
 import findIndex from 'lodash/findIndex';
 
 export default (state = [], action) => {
   switch(action.type) {
+
     case ADD_FLASH_MESSAGE:
       // prevent duplicate messages
-      const exists = findIndex(state, { text: action.message.text });
-      if (exists >= 0) {
+      const index = findIndex(state, { text: action.message.text });
+      if (index >= 0) {
         return state;
       } else {
         return [
-          ...state,
           {
-            id: shortid.generate(),
             type: action.message.type,
             text: action.message.text
           }
         ];
       }
-
-    case DELETE_FLASH_MESSAGE:
-      const index = findIndex(state, { id: action.id });
-      if (index >= 0) {
-        return [
-          ...state.slice(0, index),
-          ...state.slice(index + 1)
-        ];
-      }
-      return state;
 
     case CLEAR_FLASH_MESSAGE:
       return []
